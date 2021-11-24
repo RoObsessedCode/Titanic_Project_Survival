5882952ca825e1a3de5ef0ce078b2c344bed0b33
Rohan Saigal <rohan4saigal@gmail.com>
2021-11-24 14:25:58 -0500

    load passengers data

---
+++ passengers.py
@@ -0,0 +1,11 @@
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+passengers = pd.read_csv("passengers.csv")
+print(passengers.head())
